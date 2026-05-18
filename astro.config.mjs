1b96513a13b3343f5235431b76d81f921aa4ea70
digimedios-stream <digimediosapps@gmail.com>
2026-05-18 19:56:12 -0300

    configurar dominio personalizado en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://digimedios-stream.github.io',
-  base: '/canal2-avc-app/',
+  site: 'https://canal2avcmontecaseros.com.ar',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },

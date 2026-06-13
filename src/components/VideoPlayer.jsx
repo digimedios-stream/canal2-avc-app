0b8d9f2f6b88675978d074fe61b044c05db90951
digimedios-stream <digimediosapps@gmail.com>
2026-06-13 18:23:08 -0300

    Update mobile 480p stream URL

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -5,7 +5,7 @@ const VideoPlayer = () => {
   const videoRef = useRef(null);
   const videoSrcHD = "https://giatv.bozztv.com/giatv/giatv-digimediosstreamavc/digimediosstreamavc/playlist.m3u8";
   // Reemplaza esta URL con el enlace .m3u8 de 480p cuando lo tengas
-  const videoSrcMobile = "https://usb.bozztv.com/avchomero3/index.m3u8"; 
+  const videoSrcMobile = "https://lbgo.bozztv.com/ssh101/ssh101/avchomero3/playlist.m3u8"; 
 
   const [isNative, setIsNative] = useState(false);
   const [error, setError] = useState(null);
